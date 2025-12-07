aad59633582d03ae314b8bc8727edc4c67fb9a0e
sanseongKo <tkstjd56@naver.com>
2025-12-07 20:04:56 +0900

    [Course] Price vo 추가
    - 코스의 금액을 나타내는 vo
    - 관련 불변식 포함

---
+++ src/main/java/com/lxp/course/domain/vo/Price.java
@@ -0,0 +1,25 @@
+package com.lxp.course.domain.vo;
+
+import com.lxp.course.exception.BusinessException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.math.BigDecimal;
+
+import static com.lxp.course.domain.common.CommonStaticFieldName.ALLOWED_PRICE;
+import static com.lxp.course.domain.exception.CourseErrorCode.PRICE_MUST_MORE_THAN_ZERO;
+
+@Embeddable
+public record Price(
+    @Column(nullable = false)
+    BigDecimal price
+) {
+    public Price {
+        if (price == null || price.compareTo(BigDecimal.ZERO) <= ALLOWED_PRICE)
+            throw BusinessException.builder(PRICE_MUST_MORE_THAN_ZERO).build();
+    }
+
+    public Price getPrice() {
+        return new Price(price);
+    }
+}
