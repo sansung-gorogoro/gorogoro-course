a64174b498872ac80a132656df88f1cd3df5aef1
sanseongKo <tkstjd56@naver.com>
2025-12-09 17:54:11 +0900

    [Course] 코스 수정 서비스 메서드, id 기반 코스 찾는 메서드 추가

--- src/main/java/com/lxp/course/application/service/CourseCommandService.java
+++ src/main/java/com/lxp/course/application/service/CourseCommandService.java
@@ -1,9 +1,13 @@
 package com.lxp.course.application.service;
 
 import com.lxp.course.application.port.in.CreateCourseUseCase;
+import com.lxp.course.application.port.in.UpdateCourseUseCase;
 import com.lxp.course.application.port.in.command.CreateCourseCommand;
+import com.lxp.course.application.port.in.command.UpdateCourseCommand;
 import com.lxp.course.domain.Course;
+import com.lxp.course.domain.exception.CourseErrorCode;
 import com.lxp.course.domain.repository.CourseRepository;
+import com.lxp.course.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +15,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class CourseCommandService implements CreateCourseUseCase {
+public class CourseCommandService implements CreateCourseUseCase, UpdateCourseUseCase {
     private final CourseRepository courseRepository;
 
     @Override
     public void createExecute(CreateCourseCommand command) {
         courseRepository.save(Course.create(command.toSpec()));
+    }
+
+    @Override
+    public void updateExecute(UpdateCourseCommand command) {
+        Course course = findByIdOrThrow(command.courseId());
+
+        course.update(command.toSpec());
+    }
+
+    private Course findByIdOrThrow(Long courseId) {
+        return courseRepository.findById(courseId)
+            .orElseThrow(() ->
+                BusinessException.builder(CourseErrorCode.COURSE_NOT_FOUND)
+                    .withField(courseId.toString())
+                    .build()
+            );
     }
 }
