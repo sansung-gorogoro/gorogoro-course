31750ac9259b955aff61e3ca218242db34acd1a4
sanseongKo <tkstjd56@naver.com>
2025-12-10 14:49:54 +0900

    [Course] 코스 instructor용 서머리 조회 dto, usecase 추가

--- src/main/java/com/lxp/course/application/port/in/GetCourseUseCase.java
+++ src/main/java/com/lxp/course/application/port/in/GetCourseUseCase.java
@@ -1,9 +1,12 @@
 package com.lxp.course.application.port.in;
 
 import com.lxp.course.application.port.in.dto.CourseSummaryDto;
+import com.lxp.course.application.port.in.dto.CourseSummaryInstructorDto;
 
 import java.util.List;
 
 public interface GetCourseUseCase {
     List<CourseSummaryDto> getCoursesSummaryExecute();
+
+    List<CourseSummaryInstructorDto> getCoursesSummaryInstructorExecute(Long instructorId);
 }
