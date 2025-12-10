0f80f62f139008a08b985dc44a048aa96371f43b
sanseongKo <tkstjd56@naver.com>
2025-12-10 10:18:42 +0900

    [Course] 강사 아이디 받도록 변경

--- src/main/java/com/lxp/course/application/port/in/DeleteCourseUseCase.java
+++ src/main/java/com/lxp/course/application/port/in/DeleteCourseUseCase.java
@@ -1,5 +1,5 @@
 package com.lxp.course.application.port.in;
 
 public interface DeleteCourseUseCase {
-    void deleteCourseExecute(Long courseId);
+    void deleteCourseExecute(Long courseId, Long userId);
 }
