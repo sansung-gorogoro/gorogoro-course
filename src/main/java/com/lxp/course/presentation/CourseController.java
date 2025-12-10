7c694d8b56848bd018bc9a2dd72d129822ca9085
sanseongKo <tkstjd56@naver.com>
2025-12-10 14:24:03 +0900

    [Course] 전체 코스 서머리 조회 기능

--- src/main/java/com/lxp/course/presentation/CourseController.java
+++ src/main/java/com/lxp/course/presentation/CourseController.java
@@ -1,5 +1,6 @@
 package com.lxp.course.presentation;
 
+import com.lxp.course.application.port.in.GetCourseUseCase;
 import com.lxp.course.application.port.in.CreateCourseUseCase;
 import com.lxp.course.application.port.in.DeleteChapterUseCase;
 import com.lxp.course.application.port.in.DeleteCourseUseCase;
@@ -11,10 +12,12 @@ import com.lxp.course.presentation.request.CreateCourseRequest;
 import com.lxp.course.presentation.request.DeleteChaptersRequest;
 import com.lxp.course.presentation.request.DeleteLessonsRequest;
 import com.lxp.course.presentation.request.UpdateCourseRequest;
+import com.lxp.course.presentation.response.CourseSummaryResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +33,7 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 @RequiredArgsConstructor
 public class CourseController implements CourseApi {
     private final CreateCourseUseCase createCourseUseCase;
+    private final GetCourseUseCase getCourseUseCase;
     private final UpdateCourseUseCase updateCourseUseCase;
     private final DeleteCourseUseCase deleteCourseUseCase;
     private final DeleteChapterUseCase deleteChapterUseCase;
@@ -41,6 +45,11 @@ public class CourseController implements CourseApi {
         @Valid @RequestBody CreateCourseRequest request
     ) {
         createCourseUseCase.createExecute(request.toCommand(instructorId));
+    }
+
+    @GetMapping
+    public CourseSummaryResponse getCourses() {
+        return CourseSummaryResponse.of(getCourseUseCase.getCoursesSummaryExecute());
     }
 
     @PutMapping("/{courseId}")
