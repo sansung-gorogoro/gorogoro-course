f90c9fc007975126d3aa473e92cfd6eb67d838ea
sanseongKo <tkstjd56@naver.com>
2025-12-11 11:50:57 +0900

    [Course] 코스 상세 조회 service 코드 추가

--- src/main/java/com/lxp/course/course/application/port/in/GetCourseUseCase.java
+++ src/main/java/com/lxp/course/course/application/port/in/GetCourseUseCase.java
@@ -1,13 +1,15 @@
 package com.lxp.course.course.application.port.in;
 
+import com.lxp.course.course.application.port.in.dto.CourseDetailDto;
 import com.lxp.course.course.application.port.in.dto.CourseSummaryDto;
 import com.lxp.course.course.application.port.in.dto.CourseSummaryInstructorDto;
 
 import java.util.List;
 
 public interface GetCourseUseCase {
-    List<CourseSummaryDto> getCoursesSummaryExecute();
     List<CourseSummaryDto> getCoursesSummaryExecute(Long categoryId);
 
     List<CourseSummaryInstructorDto> getCoursesSummaryInstructorExecute(Long instructorId);
+
+    CourseDetailDto getCourseDetail(Long courseId);
 }
