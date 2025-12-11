ab028501e517bf8dd8e6da6014ce12f1f9515970
sanseongKo <tkstjd56@naver.com>
2025-12-11 11:45:45 +0900

    [Category] 카테고리 레파지토리 추가

--- src/main/java/com/lxp/course/category/domain/repository/CategoryRepository.java
+++ src/main/java/com/lxp/course/category/domain/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.lxp.course.category.domain.repository;
 import com.lxp.course.category.domain.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository {
     List<Category> findAll();
+    Optional<Category> findById(Long id);
 }
