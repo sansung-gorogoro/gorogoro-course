26b9b9f70f000a83057170f8eb195c6b87d81afa
sanseongKo <tkstjd56@naver.com>
2026-01-15 09:49:35 +0900

    [Enrollment] 해당 유저가 해당 코스를 수강했는지 확인하는 repository 메서드 추가

--- src/main/java/com/lxp/course/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/lxp/course/enrollment/repository/EnrollmentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     List<Enrollment> findAllByUserId(Long userId);
+    boolean existsByUserIdAndCourseId(Long userId, Long courseId);
 }
